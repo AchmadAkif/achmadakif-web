b5d1f4bedce10525c0dc01434b729e6754d5f86c
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2026-02-05 22:06:08 +0700

    fix(BentoGridItem): correct GitHub link for ReactMap repository

--- src/components/aceternityui/BentoGridItem/BentoGridItem.component.tsx
+++ src/components/aceternityui/BentoGridItem/BentoGridItem.component.tsx
@@ -97,7 +97,7 @@ const BentoGridItem = ({
         >
           {id === 5 && (
             <div className="flex justify-end">
-              <a href="https://github.com/AchmadAkif/ReactMap" target="_blank">
+              <a href="https://github.com/AchmadAkif/react-map" target="_blank">
                 <button className="relative lg:h-12 h-10 lg:w-12 w-10 inline-flex overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                   <span className="inline-flex h-full cursor-pointer items-center justify-center rounded-md bg-slate-950 text-sm font-medium text-white backdrop-blur-3xl">
